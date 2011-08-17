5d67b426c8c23290fea002b6112af8a6d3d916ca
Rouven Weßling <me@rouvenwessling.de>
2011-08-17 14:03:46 +0200

    JSON view for com_login
    See this tracker issue http://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_id=8103&tracker_item_id=26509

---
+++ administrator/components/com_login/views/login/view.json.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @version		$Id: view.html.php 20196 2011-01-09 02:40:25Z ian $
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License, see LICENSE.php
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.view');
+
+/**
+ * HTML View class for the Login component
+ *
+ * @package		Joomla.Administrator
+ * @subpackage	com_login
+ * @since		1.8
+ */
+class LoginViewLogin extends JView
+{
+	function display($tpl=null)
+	{
+		$r = new JObject();
+		$r->token = JUtility::getToken(true);
+		
+		echo json_encode($r);
+	}
+}
